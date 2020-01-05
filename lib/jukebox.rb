5f4b5492def1ca3819362afbc84fdba54f44cb5d
Simon Tucker <sjtucker28@gmail.com>
2020-01-05 12:24:42 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -30,7 +30,7 @@ def play(songs)
   puts "Please enter a song name or number:"
   song_to_play = gets.chomp
   if (1..9).to_a.include?(song_to_play.to_i)
-    puts "Playing #{songs[song_to_play.to_i-1]}"
+    puts "Playing #{songs[song_to_play.to_i -1]}"
   elsif songs.include?(song_to_play)
     puts "Playing #{song_to_play}"
   else
